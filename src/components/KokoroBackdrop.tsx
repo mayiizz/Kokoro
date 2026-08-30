c181376d8b723d28649ff23d20e7d725447d3a89
mayiizz <chinmayibt02@gmail.com>
2026-08-30 10:20:00 +0530

    Tune Kokoro backdrop presentation

--- src/components/KokoroBackdrop.tsx
+++ src/components/KokoroBackdrop.tsx
@@ -2,15 +2,19 @@ import kokoroArt from "@/assets/kokoro.png";
 import { cn } from "@/lib/utils";
 
 const KokoroBackdrop = ({ className }: { className?: string }) => (
-  <img
-    src={kokoroArt}
-    alt=""
+  <div
     aria-hidden
     className={cn(
-      "pointer-events-none select-none fixed inset-0 z-0 h-full w-full object-cover opacity-[0.14]",
+      "pointer-events-none fixed inset-0 z-0 overflow-hidden opacity-[0.16]",
       className,
     )}
-  />
+  >
+    <img
+      src={kokoroArt}
+      alt=""
+      className="absolute left-1/2 top-1/2 h-[36rem] w-[36rem] max-w-none -translate-x-1/2 -translate-y-1/2 object-contain"
+    />
+  </div>
 );
 
 export default KokoroBackdrop;
